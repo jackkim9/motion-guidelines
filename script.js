bf0006200b7961f54c6f3fc7995a83cd66d10e3e
jackkim9 <jackkim@hearst.com>
2016-04-06 17:19:41 -0400

    updates script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-$('#toggle-error').on('click', function(){
-  $('.notification-bar')
-    .removeClass('success')
-    .addClass('active error')
-  ;
-  
-  setTimeout(function(){
-    $('.notification-bar').removeClass('active');
-  }, 3500);
+$(document).ready(function(){
+	$('#toggle-error').on('click', function(){
+	  $('.notification-bar')
+	    .removeClass('success')
+	    .addClass('active error')
+	  ;
+	  
+	  setTimeout(function(){
+	    $('.notification-bar').removeClass('active');
+	  }, 3500);
+	});
 });
 
