fe7643f709a2ab3aa9e9ae67534982da282df2f7
jackkim9 <jackkim@hearst.com>
2016-04-06 17:13:57 -0400

    adds notification bar

---
+++ script.js
@@ -0,0 +1,11 @@
+$('#toggle-error').on('click', function(){
+  $('.notification-bar')
+    .removeClass('success')
+    .addClass('active error')
+  ;
+  
+  setTimeout(function(){
+    $('.notification-bar').removeClass('active');
+  }, 3500);
+});
+
